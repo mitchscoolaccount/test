a025faed2122868596cc9ce968ec1f9751cdbf14
mitchscoolaccount <mwest30@knox.nsw.edu.au>
2026-08-14 11:53:21 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Fill these in with your Supabase project values:
 // Supabase Dashboard -> Project Settings -> API
-window.SUPABASE_URL = "https://YOUR-PROJECT-REF.supabase.co";
-window.SUPABASE_ANON_KEY = "YOUR-ANON-PUBLIC-KEY";
+window.SUPABASE_URL = "https://ceqphjccwhzmazfckxxg.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNlcXBoamNjd2h6bWF6ZmNreHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY2NTUxODYsImV4cCI6MjEwMjIzMTE4Nn0.DT9I3X1EjA6ODtm27QdzahKELjmAKhr8THALcufVePM";
